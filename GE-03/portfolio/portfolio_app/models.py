9c94f55cf20ebc479ec3cb64b567ebaf18d4ee5c
ryttings <scott.rytting@gmail.com>
2023-10-02 22:01:53 -0600

    Added projects to portfolios and portfolios to the models and projects to the models and all of those to the admin

--- GE-03/portfolio/portfolio_app/models.py
+++ GE-03/portfolio/portfolio_app/models.py
@@ -3,10 +3,39 @@ from django.urls import reverse
 from django.utils import timezone
 
 # Create your models here.
-class Student(models.Model):
 
-    name = models.CharField(max_length=200, default=timezone.now)
-   
+class Portfolio(models.Model):
+    title = models.CharField(max_length=200, default=timezone.now)
+    contact_email = models.CharField(max_length=200, default=timezone.now)
+    is_active = models.BooleanField(default=False)
+    about = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('portfolio-detail', args=[str(self.id)])
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200, default=timezone.now)
+    description = models.TextField()
+    #portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, default = None)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('project-detail', args=[str(self.id)])
+
+class ProjectsInPortfolio(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+class Meta:
+    unique_together = ('portfolio', 'project')
+
+class Student(models.Model):
     #List of choices for majors
     MAJOR =(
         ('CSCI-BS', 'BS in Computer Science'),
@@ -22,12 +51,10 @@ class Student(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField("UCCS Email", max_length=200)
     major = models.CharField(max_length=200, choices=MAJOR)#, blank = True)
+    portfolio = models.OneToOneField(Portfolio, on_delete=models.CASCADE, null = True, default = None, blank = True)
 
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
         return reverse('student-detail', args=[str(self.id)])
-    
-    
-    
