22d4259af9a2fc6f23d77ed6367800942fe01a78
ryttings <scott.rytting@gmail.com>
2023-10-16 10:01:49 -0600

    Ge05 project/portfolio linking work

--- GE-05CCPartIEnd/portfolio/portfolio_app/views.py
+++ GE-05CCPartIEnd/portfolio/portfolio_app/views.py
@@ -13,23 +13,27 @@ def index(request):
     return render( request, 'portfolio_app/index.html',
     {'student_active_portfolios':student_active_portfolios})
 
-    portfolio_student_owner = Portfolio.objects.select_related('student').all()
-    print("active portfolio query set", portfolio_student_owner)
-    return render( request, 'portfolio_app/index.html',
-    {'portfolio_student_owner':portfolio_student_owner})
-
-    student_portfolio_projects = Project.objects.all().filter(title__icontains=)
-    print("portfolio projects query set", student_portfolio_projects)
-    return render( request, 'portfolio_app/index.html',
-    {'student_portfolio_projects':student_portfolio_projects})
 class StudentListView(generic.ListView):
     model = Student
 class StudentDetailView(generic.DetailView):
     model = Student
 class PortfolioListView(generic.ListView):
     model = Portfolio
-class PortfolioDetailView(generic.DetailView, request):
+class PortfolioDetailView(generic.DetailView):
     model = Portfolio
+    projects = Project.objects.all()
+    student_portfolio_projects = None
+    for project in projects:
+        if project.portfolio == model.title:
+            student_portfolio_projects.append(project)
+
+    def get_context_data(self, **kwargs):
+        context = super(PortfolioDetailView, self).get_context_data( **kwargs)
+        context["student_portfolio_projects"] = "Joe"
+        return context
+
+    def __str__(self):
+        return self.id
 class ProjectListView(generic.ListView):
     model = Project
 class ProjectDetailView(generic.DetailView):
